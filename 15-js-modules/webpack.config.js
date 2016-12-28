1211a0d71d7e238195eee65de6d50d3a844262b3
Willian Justen <willianjustenqui@gmail.com>
2016-12-28 14:54:49 +0000

    Add sourcemap support

--- 15-js-modules/webpack.config.js
+++ 15-js-modules/webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const nodeENV = process.env.NODE_ENV || 'production';
 
 module.exports = {
+    devtool: 'source-map',
     entry: {
         filename: './app.js'
     },
